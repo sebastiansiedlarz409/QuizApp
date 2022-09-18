dd576e7b1c92c6937f6ab030d37f7e6c0f7da79f
Sebastian Siedlarz <siedlarzsebastian409@gmail.com>
2022-09-18 14:25:33 +0200

    Add update time info

--- src/app/bottom_bar.js
+++ src/app/bottom_bar.js
@@ -4,6 +4,7 @@ function BottomBar(props){
     return( 
         <div className='bottom_bar'>
             <span className='bottom_bar_text'>Ilość dostępnych pytań: {props.questionsCount}</span>
+            <span className='bottom_bar_text_right'>Ostatnia zmiana: 18.09.2022 14:21</span>
         </div>
     );
 }
