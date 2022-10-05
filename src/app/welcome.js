73bb820a9ac91710e8118f9902440c0d838c0f9d
Sebastian Siedlarz <siedlarzsebastian409@gmail.com>
2022-10-05 09:59:17 +0200

    Update questions set

--- src/app/welcome.js
+++ src/app/welcome.js
@@ -8,7 +8,7 @@ function Welcome(props){
             Ułatwią ci one przygotowanie się do owego egzamiu. Życzymy owocnej
             nauki :)
             </p>
-            <p>Ostatnia aktualizacja: 01.10.2022 11:00</p>
+            <p>Ostatnia aktualizacja: 03.10.2022 18:00</p>
         </div>
     );
 }
