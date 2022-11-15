0bdc9b442f1062434ea10a4de5d186f834b9e0aa
Sebastian Siedlarz <siedlarzsebastian409@gmail.com>
2022-11-15 14:52:38 +0100

    noreferrer

--- src/app/document_view.js
+++ src/app/document_view.js
@@ -5,7 +5,7 @@ function DocumentView(props) {
   let items=[];
 
   for(let i = 0;i<props.files.length;i++){
-    items.push(<li className="list_item" key={i}><a className="file" href={props.files[i]["url"]} target="_blank">{props.files[i]["title"]}</a></li>)
+    items.push(<li className="list_item" key={i}><a className="file" href={props.files[i]["url"]} target="_blank" rel="noreferrer">{props.files[i]["title"]}</a></li>)
   }
 
   return (
