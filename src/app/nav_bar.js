c0d9b8d84f6e9107723b62d6ce1c594f10338c26
Sebastian Siedlarz <siedlarzsebastian409@gmail.com>
2023-12-27 15:22:00 +0100

    Hamburger mobile menu

--- src/app/nav_bar.js
+++ src/app/nav_bar.js
@@ -3,6 +3,13 @@ import "./nav_bar.css";
 import logo from "./logo.png";
 
 function NavBar(props) {
+  const toggle_mobile_menu = () => {
+    let mobile_menu = document.querySelector(".nav_bar_mobile_menu_items");
+    if(mobile_menu){
+      mobile_menu.style.display = mobile_menu.style.display === "block" ? "none" : "block";
+    }
+  }
+
   return (
     <div className="nav_bar">
       <div className="nav_bar_menu">
@@ -32,32 +39,28 @@ function NavBar(props) {
         <span className="nav_bar_title">
           <img src={logo} alt="logo" />
         </span>
-        <div class="nav_bar_menu_mobile_rows">
-          <div class="nav_bar_menu_mobile_row">
-            <span className="nav_bar_item" onClick={() => props.setView(4)}>
-              INFO GRAFIKI
-            </span>
-            <span className="nav_bar_item" style={{color:"darkgoldenrod"}} onClick={() => props.setView(3)}>
-              MATERIAŁY
-            </span>
-            <span className="nav_bar_item" onClick={() => props.setView(0)}>
-              LOSOWE P.
-            </span>
+        <button class="nav_bar_mobile_mobile_hamburger" onClick={() => toggle_mobile_menu()}>☰</button>
+      </div>
+      <div class="nav_bar_mobile_menu_items">
+          <div className="nav_bar_item" onClick={() => props.setView(4)}>
+            INFO GRAFIKI
           </div>
-          <div class="nav_bar_menu_mobile_row">
-            <span className="nav_bar_item" onClick={() => props.setView(1)}>
-              WSZYSTKIE
-            </span>
-            <span className="nav_bar_item" onClick={() => props.setView(2)}>
-              TEST
-            </span>
-            <span className="nav_bar_item" onClick={() => props.setView(5)}>
-              USTAWIENIA
-            </span>
+          <div className="nav_bar_item" style={{color:"darkgoldenrod"}} onClick={() => props.setView(3)}>
+            MATERIAŁY
+          </div>
+          <div className="nav_bar_item" onClick={() => props.setView(0)}>
+            LOSOWE P.
+          </div>
+          <div className="nav_bar_item" onClick={() => props.setView(1)}>
+            WSZYSTKIE
+          </div>
+          <div className="nav_bar_item" onClick={() => props.setView(2)}>
+            TEST
+          </div>
+          <div className="nav_bar_item" onClick={() => props.setView(5)}>
+            USTAWIENIA
           </div>
         </div>
-        
-      </div>
     </div>
   );
 }
